ccc16cf265a66e0bdf4b691ed2733a3147db61df
teo-crth <teoconrath@gmail.com>
2025-02-28 14:31:53 +0100

    services text

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -10,6 +10,30 @@ const Faq = () => {
     return (
         <div className='container-faqPage p-5'>
             <h1 className='text-center font-title text-4xl'>FAQ - Toutes vos questions sur la création d'un site par un freelance</h1>
+            <section className='container-questions'>
+                <div className="question-1">
+                    <h2 className='font-title font-bold text-2xl'>Pourquoi choisir un site internet sur-mesure ?</h2>
+                    <p className='text-lg'>
+                        Adaptabilité : Chaque projet est développé en tenant compte des particularités de votre secteur, qu’il soit luxe, tourisme, santé, finance ou éducation, afin de garantir une solution web optimale.
+                        Fonctionnalités personnalisées : J’intègre des fonctionnalités avancées qui répondent spécifiquement à vos besoins métier, telles que des formulaires personnalisés, des portails client, des systèmes de gestion de contenu (CMS) ou encore des systèmes de réservation en ligne.
+                        Design responsive : Tous mes sites sont adaptés aux mobiles et optimisés pour le référencement SEO, assurant une visibilité accrue et une expérience utilisateur fluide sur tous les appareils.
+                        Performance et sécurité : Je m’assure que chaque site soit rapide, sécurisé, et évolutif, afin de garantir une meilleure expérience utilisateur et un excellent classement SEO sur Google.
+                        Développez votre présence en ligne avec un site web sur-mesure qui reflète vos valeurs, vos services et qui répond aux attentes spécifiques de vos clients dans votre secteur d’activité. Contactez-moi dès aujourd'hui pour discuter de votre projet et obtenir un devis personnalisé pour la création de votre site internet sur-mesure.
+                    </p>
+                </div>
+                <div className="question-1">
+                    <h2 className='font-title font-bold text-2xl'>Pourquoi choisir un site WordPress personnalisé ?</h2>
+                    <p className='text-lg'>
+                        Solution clé en main : Je vous propose des sites WordPress sur-mesure, avec des thèmes et des plugins adaptés spécifiquement à votre secteur d'activité, qu'il s'agisse de commerce en ligne, de portfolio, de blog, ou de site vitrine pour votre entreprise.
+                        Optimisation SEO : Chaque site est développé en respectant les bonnes pratiques du référencement SEO afin d'améliorer votre visibilité sur Google et attirer plus de clients.
+                        Adaptabilité à votre secteur : Que vous soyez dans le tourisme, l’immobilier, les services professionnels, la restauration, ou toute autre industrie, je crée des sites WordPress parfaitement adaptés à vos besoins spécifiques.
+                        Facilité de gestion : Grâce à l’interface intuitive de WordPress, vous pouvez gérer et mettre à jour facilement votre site web, sans avoir besoin de connaissances techniques approfondies.
+                        Design responsive : Tous les sites sont optimisés pour être adaptés aux mobiles, garantissant une expérience utilisateur fluide sur tous les appareils et un meilleur classement SEO.
+                        Avec WordPress, vous pouvez évoluer facilement avec votre entreprise, en ajoutant des fonctionnalités personnalisées, des sections ou des pages spécifiques à vos besoins. Que vous ayez besoin d'un blog, d'un e-commerce avec WooCommerce, ou d'un site corporate, WordPress est une solution flexible et scalable.
+                    </p>
+                </div>
+
+            </section>
         </div>
     );
 };
