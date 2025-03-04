f1774cf858d05786c9fc74c089c04cc284e1a69b
teo-crth <teoconrath@gmail.com>
2025-03-04 11:52:10 +0100

    icon service page

--- app/tarifs/page.tsx
+++ app/tarifs/page.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTruckFast, faMobileScreenButton, faEnvelope, faPaintBrush, faChartLine, faFilter, faCreditCard, faShirt, faPercent } from '@fortawesome/free-solid-svg-icons';
+import { 
+    faTruckFast, 
+    faMobileScreenButton,
+    faEnvelope,
+    faPaintBrush, 
+    faChartLine, 
+    faFilter, 
+    faCreditCard, 
+    faShirt, 
+    faPercent, 
+    faSliders,
+    faFile,
+    faLanguage,
+    faGears,
+    faCirclePlus,
+    faShieldHalved,
+    faFloppyDisk,
+    faDownload,
+    faPenToSquare
+} from '@fortawesome/free-solid-svg-icons';
 import type { Metadata } from "next";
 import Button from '../components/ui/button';
 
@@ -18,12 +37,12 @@ const Tarifs = () => {
             <section className="container-tarifs-site flex flex-wrap items-start justify-center w-full gap-3">
                 <h2 className='font-title font-bold text-center text-4xl w-full mb-3'>Prix pour la création d'un site web</h2>
                 <article className="card-landing rounded-xl shadow-lg h-[600px] w-[350px] bg-indigo-200 relative">
-                    <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg h-[100px] w-full flex flex-col items-center justify-center'>
+                    <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg h-[100px] w-full flex flex-col items-center justify-center relative'>
                         <h3 className='font-title font-bold text-4xl'>Site vitrine</h3>
-                        <p className='font-title text-xl'>(1 page)</p>
+                        <p className='font-title text-xl absolute bottom-2'>(1 page)</p>
                     </header>
-                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center p-5'>De 450€ à 1 200€</h4>
-                    <main className='flex flex-col items-center justify-center p-7 gap-3'>
+                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center p-5 text-dark'>De 450€ à 1 200€</h4>
+                    <main className='flex flex-col items-center justify-center p-7 gap-3 text-dark'>
                         <p className='text-justify text-lg'>Présentation de votre entreprise ou projet en une seule page, claire, ergonomique et rapide à charger</p>
                         <ul className='list-tarifs font-title font-bold flex flex-col mt-5 gap-3'>
                             <div className="container-li flex items-center gap-2">
@@ -48,8 +67,8 @@ const Tarifs = () => {
                     <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg w-full h-[100px] flex flex-col items-center justify-center'>
                         <h3 className='font-title font-bold text-4xl text-center'>Site vitrine +</h3>
                     </header>
-                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center p-5'>De 750€ à 3 000€</h4>
-                    <main className='flex flex-col items-center justify-center p-7 gap-3'>
+                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center text-dark p-5 '>De 750€ à 3 000€</h4>
+                    <main className='flex flex-col items-center justify-center text-dark p-7 gap-3'>
                         <p className='text-justify text-lg'>Site de présentation de votre activité. Avec un nombre de pages et de fonctionnalités adaptées à vos besoins.</p>
                         <ul className='list-tarifs font-title font-bold flex flex-col mt-5 gap-3'>
                         <div className="container-li flex items-center gap-2">
@@ -78,8 +97,8 @@ const Tarifs = () => {
                     <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg w-full h-[100px] flex flex-col items-center justify-center'>
                         <h3 className='font-title font-bold text-4xl text-center'>Site E-commerce</h3>
                     </header>
-                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center p-5'>De 3 000€ à 7 000€</h4>
-                    <main className='flex flex-col items-center justify-center p-7 gap-3'>
+                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center text-dark p-5'>De 3 000€ à 7 000€</h4>
+                    <main className='flex flex-col items-center justify-center text-dark p-7 gap-3'>
                         <p className='text-center'>Création d'un site e-commerce avec tunnel d'achat, panier, page produit et paiement sécurisé</p>
                         <ul className='list-tarifs font-title font-bold flex flex-col mt-5 gap-3'>
                         <div className="container-li flex items-center gap-2">
@@ -105,44 +124,105 @@ const Tarifs = () => {
                     </div>
                 </article>
             </section>
-            <section className="container-tarifs-divers flex flex-col items-center justify-start w-full">
+            <section className="container-tarifs-divers flex flex-wrap items-start justify-center w-full gap-5">
                 <h2 className='font-title font-bold text-center text-4xl w-full p-10'>Prestations annexes</h2>
-                <article className='card-modification'>
+                <article className='card-modification rounded-xl shadow-lg bg-indigo-200 w-[95%] md:w-[45%] lg:md:w-[45%] xl:md:w-[45%] min-h-[510px] relative'>
                     <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg text-center'>
                         <h3 className='font-title font-bold text-2xl text-center'>Modification / Refonte d'un site existant</h3>
                     </header>
-                    <p className='text-center'>Changement graphique du site, modification structurel ou ajout de fonctionnalités.</p>
-                    <ul className='list-tarifs'>
-                        <li className='font-title font-bold text-lg'>De 200€ à 1 200€</li>
-                        <li className='font-title font-bold text-lg'>Refonte visuelle</li>
-                        <li className='font-title font-bold text-lg'>Ajout de fonctionnalités</li>
-                        <li className='font-title font-bold text-lg'>Ajout de pages</li>
-                        <li className='font-title font-bold text-lg'>Traduction en anglais</li>
-                        <li className='font-title font-bold text-lg'>Optimisation SEO</li>
-                    </ul>
+                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center text-dark p-5'>De 200€ à 1 200€</h4>
+                    <main className='flex flex-col items-center justify-center text-dark p-7 gap-3'>
+                        <p className='text-justify'>Changement graphique du site, modification structurel ou ajout de fonctionnalités.</p>
+                        <ul className='list-tarifs font-title font-bold flex flex-col mt-5 gap-3'>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faPaintBrush} /></span>
+                            <li className='text-lg'>Refonte visuelle</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faSliders} /></span>
+                            <li className='text-lg'>Ajout de fonctionnalités</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faFile} /></span>
+                            <li className='text-lg'>Ajout de pages</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faLanguage} /></span>
+                            <li className='text-lg'>Traduction en anglais</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faChartLine} /></span>
+                            <li className='text-lg'>Optimisation SEO</li>
+                        </div>
+                        </ul>
+                    </main>
+                    <div className="container-buttons flex gap-3 justify-center items-center mt-[15px] w-full absolute bottom-5">
+                        <Button type='button' className='bg-secondary hover:bg-indigo-800 px-5' text="DEVIS" router='/devis'/>
+                    </div>
                 </article>
-                <article className="card-formation">
-                    <h3 className='font-title font-bold text-2xl text-center'>Formation Wordpress</h3>
-                    <p className='text-center'>Formation à l'utilisation de Wordpress pour gérer votre site internet</p>
-                    <ul className='list-tarifs'>
-                        <li className='font-title font-bold text-lg'>De 100€ à 300€</li>
-                        <li className='font-title font-bold text-lg'>Création de contenu</li>
-                        <li className='font-title font-bold text-lg'>Ajout de pages & d'articles de blog</li>
-                        <li className='font-title font-bold text-lg'>Mise à jour de plugin</li>
-                        <li className='font-title font-bold text-lg'>Optimisation SEO</li>
-                    </ul>
+                <article className="card-formation rounded-xl shadow-lg bg-indigo-200 w-[95%] md:w-[45%] lg:md:w-[45%] xl:md:w-[45%] min-h-[510px]">
+                    <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg text-center'>
+                        <h3 className='font-title font-bold text-2xl text-center'>Formation Wordpress</h3>
+                    </header>
+                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center text-dark p-5'>De 100€ à 300€</h4>
+                    <main className='flex flex-col items-center justify-center text-dark p-7 gap-3'>
+                        <p className='text-justify'>Formation à l'utilisation de Wordpress pour gérer votre site internet</p>
+                        <ul className='list-tarifs font-title font-bold flex flex-col mt-5 gap-3'>
+                        <div className="container-li flex items-center gap-2">
+                        <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faCirclePlus} /></span>
+                            <li className='font-title font-bold text-lg'>Création de contenu</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faFile} /></span>
+                            <li className='font-title font-bold text-lg'>Ajout de pages & d'articles de blog</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faGears} /></span>
+                            <li className='font-title font-bold text-lg'>Mise à jour de plugin</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                            <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faChartLine} /></span>
+                            <li className='font-title font-bold text-lg'>Optimisation SEO</li>
+                        </div>
+                        </ul>
+                    </main>
+                    <div className="container-buttons flex gap-3 justify-center items-center mt-[15px] mb-[15px] w-full">
+                        <Button type='button' className='bg-secondary hover:bg-indigo-800 px-5' text="DEVIS" router='/devis'/>
+                    </div>
                 </article>
-                <article className="card-maintenance">
-                    <h3 className='font-title font-bold text-2xl text-center'>Maintenance ponctuelle</h3>
-                    <p className='text-center'>Mise à jour de votre site internet, sauvegarde, sécurité, etc.</p>
-                    <ul className='list-tarifs'>
-                        <li className='font-title font-bold text-lg'>30€ / heure</li>
-                        <li className='font-title font-bold text-lg'>Modification de texte</li>
-                        <li className='font-title font-bold text-lg'>Mise à jour de version</li>
-                        <li className='font-title font-bold text-lg'>Sauvegarde</li>
-                        <li className='font-title font-bold text-lg'>Sécurité</li>
-                        <li className='font-title font-bold text-lg'>Optimisation SEO</li>
-                    </ul>
+                <article className="card-maintenance  rounded-xl shadow-lg bg-indigo-200 w-[95%] md:w-[45%] lg:md:w-[45%] xl:md:w-[45%] min-h-[510px]">
+                    <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg text-center'>
+                        <h3 className='font-title font-bold text-2xl text-center'>Maintenance ponctuelle</h3>
+                    </header>
+                    <h4 className='font-title font-bold text-2xl md:text-4xl lg:text-4xl xl:text-4xl text-center text-dark p-5'>30€ / heure</h4>
+                    <main className='flex flex-col items-center justify-center text-dark p-7 gap-3'>
+                        <p className='text-center'>Mise à jour de votre site internet, sauvegarde, sécurité, etc.</p>
+                        <ul className='list-tarifs font-title font-bold flex flex-col mt-5 gap-3'>
+                        <div className="container-li flex items-center gap-2">
+                        <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faPenToSquare} /></span>
+                            <li className='font-title font-bold text-lg'>Modification de texte</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                        <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faDownload} /></span>
+                            <li className='font-title font-bold text-lg'>Mise à jour de version</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                        <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faFloppyDisk} /></span>
+                            <li className='font-title font-bold text-lg'>Sauvegarde</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                        <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faShieldHalved} /></span>
+                            <li className='font-title font-bold text-lg'>Sécurité</li>
+                        </div>
+                        <div className="container-li flex items-center gap-2">
+                        <span className='w-10 flex justify-center'><FontAwesomeIcon icon={faChartLine} /></span>
+                            <li className='font-title font-bold text-lg'>Optimisation SEO</li>
+                        </div>
+                        </ul>
+                    </main>
+                    <div className="container-buttons flex gap-3 justify-center items-center mt-[15px] mb-[15px] w-full">
+                        <Button type='button' className='bg-secondary hover:bg-indigo-800 px-5' text="DEVIS" router='/devis'/>
+                    </div>
                 </article>
             </section>
         </div>
