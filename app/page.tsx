24706dee5c98f611fb676ed201043483db9a1992
teo-crth <teoconrath@gmail.com>
2025-03-10 11:57:04 +0100

    fix size articles home

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   return (
     <div className="container-homePage pb-20 gap-16 w-full">
-      <section className="presentation m-15 flex flex-col justify-center items-center">
+      <section className="presentation p-15 w-full flex flex-col justify-center items-center">
         <h1 className="text-center font-bold font-title md:text-6xl lg:text-6xl xl:text-6xl text-4xl text-secondary text-shadow-white">Création de sites web sur-mesure</h1>
         <h2 className="text-center font-bold font-title md:text-2xl lg:text-2xl xl:text-2xl text-xl text-dark z-2">Développeur freelance à Aix-en-Provence</h2>
         <h3 className="text-center font-bold font-title md:text-xl lg:text-xl xl:text-xl text-lg text-gray-400 z-2">Site vitrine, Blog, Site E-commerce</h3>
@@ -40,7 +40,7 @@ export default function Home() {
       <Separator type="2" />
       <section className="container-quiSuisJe flex flex-col items-center justify-center gap-5 pb-20 px-5">
         <h2 className="font-bold font-title md:text-6xl lg:text-6xl xl:text-6xl text-4xl text-secondary z-2 text-center text-shadow-white">Mes services</h2>
-        <div className="container-illustration w-[50%] md:w-[350%] lg:w-[25%] xl:w-[15%] p-5 z-2 bg-light/35 rounded-full">
+        <div className="container-illustration w-[50%] md:w-[35%] lg:w-[25%] xl:w-[15%] p-5 z-2 bg-light/35 rounded-full">
           <img className="w-full h-full object-cover" src="/images/illustration-4.png" alt="illustration colorée d'une personne en position de super-héro devant une page web" />
         </div>
         <div className="container-cards-services w-full flex flex-wrap justify-center items-center gap-3 z-2 mt-7">
@@ -50,12 +50,13 @@ export default function Home() {
                 <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg h-[100px] w-full flex flex-col items-center justify-center relative'>
                   <h3 className='font-title font-bold text-center text-2xl md:text-4xl'>Création de site</h3>
                 </header>
-                <ul className='text-lg font-title font-bold tracking-wider text-light text-center flex justify-center items-start p-5 flex-wrap gap-1'>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Charte graphique</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Performance</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Responsive</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Optimisation SEO</li>
-                </ul>
+                <div className="container-tags flex flex-col justify-center items-center gap-2 w-full min-h-[180px]">
+                  <ul className='text-lg font-title font-bold tracking-wider text-light text-center flex justify-center items-start p-5 flex-wrap gap-1'>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Charte graphique</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Responsive</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Optimisation SEO</li>
+                  </ul>
+                </div>
               </div>
             </article>
           </Link>
@@ -65,28 +66,32 @@ export default function Home() {
                 <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg h-[100px] w-full flex flex-col items-center justify-center relative'>
                   <h3 className='font-title font-bold text-center text-2xl md:text-4xl'>Modification de site</h3>
                 </header>
-                <ul className='text-lg font-title font-bold tracking-wider text-light text-center flex justify-center items-center flex-wrap p-5 gap-1'>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Refonte graphique</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Ajout de pages</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Performance</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Contenus</li>
-                </ul>
+                <div className="container-tags flex flex-col justify-center items-center gap-2 w-full min-h-[180px]">
+                  <ul className='text-lg font-title font-bold tracking-wider text-light text-center flex justify-center items-center flex-wrap p-5 gap-1'>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Refonte graphique</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Ajout de pages</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Performance</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Contenus</li>
+                  </ul>
+                </div>
               </div>
             </article>
           </Link>
           <Link href="/tarifs" className="cursor-pointer z-2 w-[95%] md:w-[32%] lg:w-[25%] xl:w-[20%]" aria-label="Navigation vers la page des tarifs">
             <article className='cards-services flex justify-center items-center'>
-              <div className="card bg-white/25 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-lg border rounded-xl border-white/18 w-full flex flex-col justify-center items-center gap-2">
+              <div className="card bg-white/25 shadow-[0_8px_32px_0_rgba(31,38,135,0.37)] backdrop-blur-lg border rounded-xl border-white/18 w-full flex flex-col justify-start items-center gap-2">
                 <header className='header-card bg-secondary text-light rounded-t-xl p-3 shadow-lg h-[100px] w-full flex flex-col items-center justify-center relative'>
                   <h3 className='font-title font-bold text-center text-2xl md:text-4xl'>Formation</h3>
                 </header>
-                <ul className='text-lg font-title font-bold tracking-wider text-light text-center flex justify-center items-center flex-wrap p-5 gap-1'>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Wordpress</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>HTML / CSS</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Javascript</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Outils</li>
-                  <li className='bg-secondary rounded-full px-3 py-0.5'>Hébergement</li>
-                </ul>
+                <div className="container-tags flex flex-col justify-center items-center gap-2 w-full min-h-[180px]">
+                  <ul className='text-lg font-title font-bold tracking-wider text-light text-center flex justify-center items-center -py-2 flex-wrap p-5 gap-1'>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Wordpress</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>HTML / CSS</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Javascript</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Outils</li>
+                    <li className='bg-secondary rounded-full px-3 py-0.5'>Hébergement</li>
+                  </ul>
+                </div>
               </div>
             </article>
           </Link>
